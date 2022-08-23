5d318fafe5c7d6022d470f152987128c9fd3ce47
Meriem Arbaoui <m.arbaoui@esi-sba.dz>
2022-08-23 23:46:17 +0100

    Single Page Application Part2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -21,7 +21,7 @@ class Header extends Component {
     render() {
         return (
             <React.Fragment>
-            <Navbar dark navbar expand="md">
+            <Navbar dark  expand="md">
                 <div className="container">
                     <NavbarToggler onClick={this.toggleNav}/>
                     <NavbarBrand className='mr-auto' href="/">
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,13 +2,14 @@ import {React, Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContacteComponenet';
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions'
 import { LEADERS } from '../shared/leaders';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useParams} from 'react-router-dom';
 
 
 class Main extends Component {
@@ -37,12 +38,22 @@ class Main extends Component {
           />
       );
       }
+
+    const DishWithID = () =>{
+       //  get ID from url
+      const params = useParams();
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(params.dishId, 10))[0]}
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(params.dishId, 10))} />
+        );
+    }
     return (
         <div>
             <Header />
             <Routes>
                 <Route path='/home' element={<HomePage/>}/>
                 <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />} />
+                <Route path='/menu/:dishId' element={<DishWithID />}/>
                 <Route exact path='/contactus' element={<Contact />}/>
                 <Route path='*' element={<HomePage/>}/>
             </Routes>
