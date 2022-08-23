7957e0c2916eff7b3b4bc0ba5393590ca67ed6de
Meriem Arbaoui <m.arbaoui@esi-sba.dz>
2022-08-23 22:59:46 +0100

    Single Page Application Part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,13 @@
 import {React, Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContacteComponenet';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotions'
+import { LEADERS } from '../shared/leaders';
 import {Routes, Route} from 'react-router-dom';
 
 
@@ -13,7 +17,10 @@ class Main extends Component {
     super(propos);
 
     this.state = {
-      dishes : DISHES
+      dishes : DISHES,
+      comments : COMMENTS,
+      leaders : LEADERS,
+      promotions : PROMOTIONS
     };
   }
 
@@ -24,7 +31,9 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-          <Home 
+          <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
       }
@@ -34,6 +43,7 @@ class Main extends Component {
             <Routes>
                 <Route path='/home' element={<HomePage/>}/>
                 <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />} />
+                <Route exact path='/contactus' element={<Contact />}/>
                 <Route path='*' element={<HomePage/>}/>
             </Routes>
             <Footer/>
